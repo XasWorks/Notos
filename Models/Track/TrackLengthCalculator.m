7a2537167502c90c3af115c2aa0567c12b841ee7
David Bailey <davidbailey.2889@gmail.com>
2016-09-11 14:52:13 +0200

    Hotfix für den TrackLengthCalculator.

--- Models/Track/TrackLengthCalculator.m
+++ Models/Track/TrackLengthCalculator.m
@@ -7,18 +7,29 @@ wheelPositions = [[0, 40]
 						[-15, 10]
 						[15, 10]
 						[45, 10]];
-wheelSizes = [15, 10, 10, 10, 10, 10];
+wheelSizes = [10, 10, 10, 10, 10, 10];
+
+function aDeg = radToDec(aDeg)
+	aDeg = aDeg * 180 / pi;
+endfunction
+
+function a = aBetweenA(aA, aB)
+  if(aB < aA) aB += 2*pi; endif
+
+  a = aB - aA;
+endfunction
 
 function l = lengthAroundWheel(wheelNum, angle)
   global wheelSizes;
   l = abs(wheelSizes(wheelNum) * angle);
+  printf("Angle: %f, Length: %f\n\n", radToDec(angle), l);
 endfunction
 
 function [d, a] = lineDetails(wheelA, wheelB)
   global wheelPositions wheelSizes;
-  
+
   conVect = wheelPositions(wheelB,:) - wheelPositions(wheelA,:);
-  initAngle = atan2(conVect(1), conVect(2));
+  initAngle = atan2(conVect(2), conVect(1));
 
   l = norm(conVect);
   rDiff = wheelSizes(wheelB) - wheelSizes(wheelA);
@@ -27,8 +38,8 @@ function [d, a] = lineDetails(wheelA, wheelB)
 
   a = initAngle - extrAngle;
   d = l * cos(extrAngle);
-  
-    printf("Calculated the details for wheels %i to %i!\n Wheel-Wheel angle: %f - distance: %f\n Tanget angle: %f - length: %f\n\n", wheelA, wheelB, initAngle, l, a, d);
+
+    printf("Calculated the details for wheels %i to %i!\n Wheel-Wheel angle: %f - distance: %f\n Tanget angle: %f - length: %f\n", wheelA, wheelB, radToDec(initAngle), l, radToDec(a), d);
 endfunction
 
 function i = arLoop(n)
@@ -43,8 +54,8 @@ lastAngle = 0;
 [null, lastAngle] = lineDetails(length(wheelSizes), 1);
 
 for i=(1:length(wheelSizes))
-  [d, a] = lineDetails(arLoop(i), arLoop(i+1));
-  totalDistance = totalDistance + d + lengthAroundWheel(i, lastAngle - a);
+  [d, a] = lineDetails(i, arLoop(i+1));
+  totalDistance = totalDistance + d + lengthAroundWheel(i, aBetweenA(lastAngle, a));
 
   lastAngle = a;
 endfor
