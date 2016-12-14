e75a36cfbaa5467f5c590e0db361b5d36188be77
Xasin <logger89@web.de>
2016-12-14 13:55:32 +0100

    Hinzufügen eines LF-Skelets

---
+++ EclipseWS/Notos_Main/Localcode/Modules/LineFollow.cpp
@@ -0,0 +1,16 @@
+/*
+ * LineFollow.cpp
+ *
+ *  Created on: Nov 23, 2016
+ *      Author: xasin
+ */
+
+#include "LineFollow.h"
+#include "../Robot.h"
+
+
+namespace Modules {
+namespace LF {
+
+}
+}
